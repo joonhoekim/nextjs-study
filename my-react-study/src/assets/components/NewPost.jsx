6f4359c1a8a7b813dbf4ed86a915908889b9651f
Joonhoe Kim <26rote@gmail.com>
2024-07-04 18:05:18 +0900

    State를 통해 스냅샷 변경사항 발견되면 리액트가 DOM 바뀐 부분만 리렌더링

--- my-react-study/src/assets/components/NewPost.jsx
+++ my-react-study/src/assets/components/NewPost.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import classes from './NewPost.module.css';
+
 const NewPost = () => {
+
+  const [currentValue, setCurrentValue] = useState('');
+
+  const changeBodyHandler = (event) => {
+    setCurrentValue(event.target.value);
+  }
+
   return (
-    <>
-    <h2>NewPost</h2>
-    </>
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+      </p>
+      <p>{currentValue}</p>
+      <p>
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
   )
 }
 
--- my-react-study/src/assets/components/PostList.jsx
+++ my-react-study/src/assets/components/PostList.jsx
@@ -1,16 +1,19 @@
 import NewPost from "./NewPost"
 import Post from "./Post"
+import classes from "./PostsList.module.css"
 
 export const PostList = () => {
 
   return (
     <>
-    <NewPost />
     <h1>LIST</h1>
-    <Post author='author' body='body' />
+    <NewPost />
+    <div className={classes.posts}>
+    <Post  author='author' body='body' />
     <Post author='tester1' body='tested!!' />
     <Post author='tester2' body='tested!!' />
     <Post author='tester3' body='tested!!' />
+    </div>
     </>
   )
 }
