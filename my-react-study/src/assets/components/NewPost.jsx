3be5551e62a67895324ee70832d197789bf64bdc
Joonhoe Kim <26rote@gmail.com>
2024-07-04 21:51:43 +0900

    31-form handler

--- my-react-study/src/assets/components/NewPost.jsx
+++ my-react-study/src/assets/components/NewPost.jsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-const NewPost = ({onBodyChange, onAuthorChange, onCancel}) => {
+const NewPost = ({onCancel}) => {
+  const [author, setAuthor] = useState('');
+  const [body, setBody] = useState('');
+  const authorHandler = (event) => {
+    setAuthor(event.target.value);
+  }
+
+  const bodyHandler = (event) => {
+    setBody(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" required onChange={onAuthorChange}/>
+        <input type="text" id="name" required onChange={authorHandler}/>
       </p>
       <p className={classes.actions}>
         <button type='button' onClick={onCancel}>Cancel</button>
-        <button type='button'>Submit</button>
+        <button>Submit</button>
       </p>
 
     </form>
--- my-react-study/src/assets/components/PostList.jsx
+++ my-react-study/src/assets/components/PostList.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -6,25 +5,12 @@ import classes from "./PostsList.module.css";
 
 export const PostList = ({isPosting, onStopPosting}) => {
 
-  
-  const [author, setAuthor] = useState('');
-  const [body, setBody] = useState('');
-
-  const authorHandler = (event) => {
-    setAuthor(event.target.value);
-  }
-
-  const bodyHandler = (event) => {
-    setBody(event.target.value);
-  }
-
   return (
     <>
     {isPosting && (<Modal onClose={onStopPosting}>
-      <NewPost onBodyChange={bodyHandler} onAuthorChange={authorHandler} onCancel={onStopPosting} />
+      <NewPost onCancel={onStopPosting} />
     </Modal>) }
     <div className={classes.posts}>
-    <Post author={author} body={body} />
     <Post author='tester1' body='tested!!' />
     <Post author='tester2' body='tested!!' />
     <Post author='tester3' body='tested!!' />
